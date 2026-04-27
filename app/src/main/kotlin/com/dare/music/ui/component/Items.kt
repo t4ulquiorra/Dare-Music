84ac1a458aaee10b50fde60e77a351dd3b302ab1
t4ulquiorra <t4.ulquiorra@email.com>
2026-04-28 03:48:49 +0530

    fix: add Arrangement import for GlassGridItem

--- app/src/main/kotlin/com/dare/music/ui/component/Items.kt
+++ app/src/main/kotlin/com/dare/music/ui/component/Items.kt
@@ -16,6 +16,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.border
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
